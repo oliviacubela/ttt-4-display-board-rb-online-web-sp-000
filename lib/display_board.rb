795b144cb2b693cd907f8adcc4616203fec62917
Olivia Cubela <oliviacubela@gmail.com>
2020-05-27 14:46:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
   def display_board(board)
 separator = "|"
 lines = "-----------"
@@ -7,5 +8,14 @@ puts "#{lines}"
 puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
 puts "#{lines}"
 puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
+=======
+def display_board(board)
+  board = ["X","X","X"," "," "," "," "," "," "]
+puts " X | X | X "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+>>>>>>> bfe74cb7bea17f625b14156166a6158960901bb7
 end
 
